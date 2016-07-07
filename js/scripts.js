2e13c701cfad577646e798e46f5fed38569f9f62
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-07-06 23:07:07 -0700

    add landing page with functioning js animation

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// BACKEND
+
+
+
+// FRONTEND
+$(document).ready(function() {
+
+  $("#landing_page").fadeIn(400).delay(800).slideUp(1000).delay(400);
+
+  $("#region_hub").delay(1600).fadeIn(800);
+
+});
